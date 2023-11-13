1270f34920be82eb7edf7d310cc42d9407169b05
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-11-13 11:02:48 +0100

    versie 1
    fwjefgw

--- pixels_naar_golflengtes.py
+++ pixels_naar_golflengtes.py
@@ -6,3 +6,22 @@ Created on Mon Nov 13 10:55:18 2023
 """
 
 # probeersel om pixels om te rekenen naar golflengtes
+def pixel_to_wavelength(pixel_position, a, b):
+    wavelength = a * pixel_position + b
+    return wavelength
+
+# Voorbeeld kalibratiecoëfficiënten (moet worden aangepast met echte waarden)
+a = 0.1
+b = 500
+
+# Pixelpositie die je wilt omrekenen
+pixel_position = 100
+
+# Omrekenen naar golflengte
+wavelength = pixel_to_wavelength(pixel_position, a, b)
+
+print(f"Pixelpositie: {pixel_position}")
+print(f"Golflengte: {wavelength} nm")
+
+############################
+# testest
