43cba9b73daea97138e47c0287cfeed19e09de38
Gideon <gideonkokosky@gmail.com>
2023-11-13 11:23:42 +0100

    acquire neon data from all sets and visualize

--- neon_lines.py
+++ neon_lines.py
@@ -4,12 +4,15 @@ Data analysis ...
 
 from astropy.io import fits
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 neon_set = []
-for i in range(1,2):
+for i in range(1,10):
     
     data = fits.getdata(f'/home/gideon/Documents/NSP2/LISA data/Verschillende hoogtes/Sky_angles/calibration/neon-00{i}.fit')
     neon_set.append(data)
     
-print(neon_set)
+plt.imshow(neon_set[1], cmap = 'gray', norm='log')
+
+print(neon_set[0])
