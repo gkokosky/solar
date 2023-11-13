f049b4660830a236c1a1fbac411ebb6c9e0fd78d
Gideon <gideonkokosky@gmail.com>
2023-11-13 11:45:56 +0100

    made 1d graph

--- neon_lines.py
+++ neon_lines.py
@@ -13,6 +13,14 @@ for i in range(1,10):
     data = fits.getdata(f'/home/gideon/Documents/NSP2/LISA data/Verschillende hoogtes/Sky_angles/calibration/neon-00{i}.fit')
     neon_set.append(data)
     
-plt.imshow(neon_set[1], cmap = 'gray', norm='log')
+#plt.imshow(neon_set[1], cmap = 'gray', norm='log')
 
-print(neon_set[0])
+data = neon_set[0]
+print(data)
+data = data[:,700:1400]
+
+intensity_profile = np.sum(data, axis=0)
+print(intensity_profile)
+plt.plot(intensity_profile)
+
+neon_lines = []
