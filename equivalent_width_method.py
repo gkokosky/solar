522d235b867f76c6795278988778e489a564df7a
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-11-19 17:03:12 +0100

    data toegevoegd

--- equivalent_width_method.py
+++ equivalent_width_method.py
@@ -30,6 +30,65 @@ from specutils import SpectralRegion
 from specutils.analysis import equivalent_width
 from specutils.analysis import fwhm
 
+# DATA IMPORTEREN
+
+
+# SPECTRUM PLOT
+def interp(w, f):
+  wInterp = np.linspace(w.min(),w.max(), 300)
+  spl = make_interp_spline(w, f)
+  fInterp = spl(wInterp)
+  return wInterp, fInterp
+
+# Interpolate the data for smoother plots
+wave1, flux1 = interp(wave1,flux1)
+
+# Add units to the fluxes and wavelengths
+flux1 = flux1*u.Unit('erg cm-2 s-1 AA-1')
+wave1 = wave1*u.AA
+fig, ax = plt.subplots()
+fig.suptitle('Eu II Absorption Detection', fontsize='24')
+
+# Add the other plots here
+ax.plot(wave1, flux1, label=label1)
+
+# This displays 2 lines to mark Ni I and Eu II line locations.
+plt.axvline(x=6645.127,ls=':')
+plt.axvline(x=6643.638,ls=':')
+
+# This labels the x-axis and y-axis
+plt.xlabel('λ (Å)',fontsize='20')
+plt.ylabel('Relative flux', fontsize='20')
+
+# Display a grid
+plt.grid(True)
+
+# Turn on the legend.
+ax.legend(loc='best')
+
+# Display all the things we've setup.
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Rechthoek maken
 # Plot one of our stars and annotate the equivalent width.
 fig2, ax2 = plt.subplots()
 fig2.suptitle('Ni I Equivalent Width', fontsize='24')
@@ -53,3 +112,20 @@ ax2.add_patch(plt.Rectangle((.29, .045), .358, 0.85, ec='k', fc="k",
 plt.axhline(y=1,ls='--',color='red',lw=2)
 
 plt.show()
+
+# EQUIVALENT WIDTH BEPALEN 
+# hierbij word gebruik gemaakt van de specutils package
+
+# Calculate the EW of Ni I and Eu II in their specific spectral regions.
+ni1 = equivalent_width(spec1, regions=SpectralRegion(6643.0*u.AA,6644*u.AA))
+eu1 = equivalent_width(spec1, regions=SpectralRegion(6645.0*u.AA,6645.5*u.AA))
+r1 = eu1/ni1
+
+# Rounding
+ni1 = np.round(ni1,3)
+eu1 = np.round(eu1,3)
+r1 = np.round(r1,3)
+
+# Print the EW or Ni I, Eu II, and their ratio.
+print('EW Ni I 6643A\tEW Eu II 6645 A\tEu/Ni\tName') # Print a header row
+print(str(ni1)+'\t'+str(eu1)+'\t'+str(r1)+'\t'+label1) # Print for target 1
