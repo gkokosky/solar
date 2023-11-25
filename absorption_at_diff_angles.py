94b5366d751b6731e687cb01230268557ccec8e3
Gideon <gideonkokosky@gmail.com>
2023-11-25 23:18:41 +0100

    ehh

--- absorption_at_diff_angles.py
+++ absorption_at_diff_angles.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-angles = ['06', '10', '15', '30', '40', '50', '60', '70', '80', '90']
 
 # returns area from 10 measurements at 1 angle
 def ten_mesurements(degrees, min, max, wavelength, width, smoothing, small_min, small_max):
@@ -16,15 +15,38 @@ def ten_mesurements(degrees, min, max, wavelength, width, smoothing, small_min, 
             measurement = f'00{i}'
         elif len(measurement) == 2:
             measurement = f'0{i}'
+        else:
+            print('huh')
 
         measure = Area(degrees, measurement, min, max, wavelength, width, smoothing)
         measure.isolate(small_min, small_max)
         x,y = measure.drop()
-        
         area = measure.trap()
         area_list.append(area)
         
     avg = np.mean(area_list)
     err = np.std(area_list) / np.sqrt(10)
     
-    return area_list, avg, err
+    return np.array(area_list), avg, err
+
+def angles(min, max, wavelength, width, smoothing, small_min, small_max):
+    
+    angles = ['06', '10', '15', '30', '40', '50', '60', '70', '80', '90']
+    
+    area_array = []
+    avg_list = []
+    err_list = []
+    for i in angles:
+        area, avg, err = ten_mesurements(i, min, max, wavelength, width, smoothing, small_min, small_max)
+        area_array.append(area)
+        avg_list.append(avg)
+        err_list.append(err)
+    
+    angles = np.array(angles)
+    angles = angles.astype(int)
+    print(angles)
+    avg = np.array(avg_list)
+    err = np.array(err_list)
+    
+    plt.figure()
+    plt.errorbar(angles, avg, yerr=err, fmt='o')
