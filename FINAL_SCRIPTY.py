70d4c2f24427dcb736273acca9a7939d13e2c7f7
FemkeFVB <149329792+FemkeFVB@users.noreply.github.com>
2023-12-04 13:11:24 +0100

    added plt.show() bc I work on windows

--- FINAL_SCRIPTY.py
+++ FINAL_SCRIPTY.py
@@ -46,33 +46,36 @@ plt.xlim(0,90)
 plt.ylim(0,0.5)
 plt.ylabel('oppervlakte spectraallijn')
 plt.rcParams['figure.dpi'] = 300
+plt.show()
 
-# wavelength = 493
-# corr = 10 
-# small_min_corr = 1
-# small_max_corr = 1
+wavelength = 493
+corr = 10 
+small_min_corr = 1
+small_max_corr = 1
 
-# x,y,err,result = graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
-
-
-# plt.errorbar(x, y, yerr=err, fmt='o')
-# plt.plot(x, result.best_fit)
-# plt.rcParams['figure.dpi'] = 300
-# plt.xlim(0,90)
-# plt.ylim(0,0.5)
+x,y,err,result = graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
 
 
-# wavelength = 656
-# corr = 10
-# small_min_corr = 1
-# small_max_corr = 2
-# graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
+plt.errorbar(x, y, yerr=err, fmt='o')
+plt.plot(x, result.best_fit)
+plt.rcParams['figure.dpi'] = 300
+plt.xlim(0,90)
+plt.ylim(0,0.5)
+plt.show()
 
-# x,y,err,result = graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
+
+wavelength = 656
+corr = 10
+small_min_corr = 1
+small_max_corr = 2
+graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
+
+x,y,err,result = graph_and_fit(wavelength, corr, small_min_corr, small_max_corr)
 
 
-# plt.errorbar(x, y, yerr=err, fmt='o')
-# plt.plot(x, result.best_fit)
-# plt.rcParams['figure.dpi'] = 300
-# plt.xlim(0,90)
-# plt.ylim(0,0.5)
+plt.errorbar(x, y, yerr=err, fmt='o')
+plt.plot(x, result.best_fit)
+plt.rcParams['figure.dpi'] = 300
+plt.xlim(0,90)
+plt.ylim(0,0.5)
+plt.show()
